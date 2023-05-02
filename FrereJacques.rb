8e82251d95629a2d59151ed2553ad769f0ca9296
Mr-Partington <131898297+Mr-Partington@users.noreply.github.com>
2023-05-02 10:23:18 +0100

    Create FrereJacques.rb

---
+++ FrereJacques.rb
@@ -0,0 +1,42 @@
+use_bpm 250
+use_synth :dtri
+2.times do
+  play 60
+  sleep 1
+  play 62
+  sleep 1
+  play 64
+  sleep 1
+  play 60
+  sleep 1
+end
+2.times do
+  play 64
+  sleep 1
+  play 65
+  sleep 1
+  play 67
+  sleep 2
+end
+2.times do
+  play 67
+  sleep 0.5
+  play 69
+  sleep 0.5
+  play 67
+  sleep 0.5
+  play 65
+  sleep 0.5
+  play 64
+  sleep 1
+  play 60
+  sleep 1
+end
+2.times do
+  play 60
+  sleep 1
+  play 55
+  sleep 1
+  play 60
+  sleep 2
+end
